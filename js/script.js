ecce1389a2182ec8106ce152741f996c372b2987
Edvige Di Salvo <edvigedis@google.com>
2024-11-04 15:56:43 +0100

    window.onload is set

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//Script.js, houses the events listeners + make the game keyboard responsive
+
+/*When you use window.onload = function () { ... }, you’re telling the browser to wait 
+until everything on the page has loaded, and then execute the code inside the function.*/
+
+window.onload = function () {
+    //declared globally to allow the start of the game
+    let myGame;
+  const startBtn = document.getElementById("start-button");
+
+  startBtn.addEventListener("click", function () {
+    startGame();
+  });
+}
+
+function startGame() {
+    console.log("start game");
+    //this has to be made global
+    let myGame = new Game();
+    myGame.start();
+}
