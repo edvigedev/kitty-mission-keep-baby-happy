5a6523d4bba55707be2e6eaf485a28433ad2e365
Edvige Di Salvo <edvigedis@google.com>
2024-11-06 17:02:50 +0100

    add projectile

---
+++ js/projectile.js
@@ -0,0 +1,54 @@
+class Projectile {
+  constructor(positionX, positionY) {
+    this.gameScreen = document.getElementById("game-screen");
+
+    // Define width and height for the obstacle
+    this.width = 180;
+    this.height = 150;
+
+    // Set the initial position at the top of the screen
+    this.top = positionY; //
+    this.left = positionX;
+
+    // Dynamically set the left position based on the screen width
+    const screenWidth = this.gameScreen.clientWidth;
+    this.left = Math.floor(Math.random() * (screenWidth - this.width)); // Random position across screen width
+
+    //Array of image paths for different obstacle images
+    const images = [
+      "/kitty-mission-keep-baby-happy/images/panettone.png",
+      "/kitty-mission-keep-baby-happy/images/coffee.png",
+      "/kitty-mission-keep-baby-happy/images/chocolate.png",
+    ];
+    // Select a random image for the obstacle
+    const randomImageIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomImageIndex];
+
+    //.src adds the path to the image, since it is an argument we can
+    //change it, aka different cars every time the game starts
+    //creating the obstacle
+    this.element = document.createElement("img");
+    this.element.src = selectedImage;
+    this.element.style.position = "absolute";
+    this.element.style.height = `${this.height}px`;
+    this.element.style.width = `${this.width}px`;
+    this.element.style.top = `${this.top}px`;
+    this.element.style.left = `${this.left}px`;
+
+    //add the obstacle to the screen
+    this.gameScreen.appendChild(this.element);
+  }
+  // it does not have directionX or directionY, only falls down
+  move() {
+    //this is non-responsive to keyboard and it always has to be falling.
+    this.top -= 10;
+    this.updatePosition();
+  }
+
+  updatePosition() {
+    this.element.style.top = `${this.top}px`;
+  }
+  isOutofScreen() {
+    return this.left > this.gameScreen.clientWidth;
+  }
+}
